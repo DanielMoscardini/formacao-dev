1914a71cf39cfd02051b138d7e028bf40bc4ef05
DANIEL BOLONHA MOSCARDINI <moscardinibdaniel@gmail.com>
2023-07-03 19:54:21 -0300

    Iniciando curso de tailwind

---
+++ especialista/tailwind/react-tailwind/src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div
+      className={`
+     flex justify-center items-center h-screen
+     bg-black text-7xl text-white`}
+    >
+      Pagina inicial
+    </div>
+  );
+}
